4c4a6a5580f26c898cf67029c8cebff4b3e3fea6
koryou <90591869+k0ryou@users.noreply.github.com>
2023-03-30 04:05:17 +0900

    add: main test case

--- main_test.go
+++ main_test.go
@@ -19,6 +19,26 @@ func TestConvertToHTMLString(t *testing.T) {
 			args: args{markdown: "text**strong**\n- hoge\n- li\n1. ge"},
 			want: "text<strong>strong</strong><ul><li>hoge</li><li>li</li></ul><ol><li>ge</li></ol>",
 		},
+		{
+			name: "strong_ok1",
+			args: args{markdown: "**foo bar**"},
+			want: "<strong>foo bar</strong>",
+		},
+		{
+			name: "strong_bad1",
+			args: args{markdown: "** foo bar**"},
+			want: "** foo bar**",
+		},
+		{
+			name: "strong_bad2",
+			args: args{markdown: 'a**"foo"**'},
+			want: "a**&quot;foo&quot;**",
+		},
+		{
+			name: "strong_ok2",
+			args: args{markdown: "foo**bar**"},
+			want: "foo<strong>bar</strong>",
+		},
 	}
 
 	for _, tt := range tests {
